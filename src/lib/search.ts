0953f5a4d33b27aa6898420c384594832b579de9
teauxfu <teauxfu@icloud.com>
2025-12-16 19:26:40 -0600

    fix imports, some formatting and null checks

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,58 +1,58 @@
-import FlexSearch from 'flexsearch';
-import type {  SearchItem } from "$lib/types";
+import { Index } from "flexsearch";
+import type { SearchItem } from "$lib/types";
 
-let postsIndex: FlexSearch.Index;
+let postsIndex: Index;
 let posts: SearchItem[];
 
 export function createPostsIndex(data: SearchItem[]) {
-    postsIndex = new FlexSearch.Index({tokenize: 'forward'});
+	postsIndex = new Index({ tokenize: "forward" });
 
-    data.forEach((post, i) => {
-        const item = `${post.category} ${post.title} ${post.text}`;
-        postsIndex.add(i, item);
-    });
+	data.forEach((post, i) => {
+		const item = `${post.category} ${post.title} ${post.text}`;
+		postsIndex.add(i, item);
+	});
 
-    posts = data;
+	posts = data;
 }
 
-export function searchPostsIndex(searchTerm: string)  {
-    const match = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    const results = postsIndex.search(match);
-    
-    return results
-        .map((index) => posts[index as number])
-        .map(({ slug, category, title, text }) => {
-            return {
-                slug,
-                category: replaceTextWithMarker(category, match),
-                title: replaceTextWithMarker(title, match),
-                text: getMatches(text, match)
-            }
-        });
+export function searchPostsIndex(searchTerm: string) {
+	const match = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+	const results = postsIndex.search(match);
+
+	return results
+		.map((index) => posts[index as number])
+		.map(({ slug, category, title, text }) => {
+			return {
+				slug,
+				category: replaceTextWithMarker(category ?? "", match),
+				title: replaceTextWithMarker(title ?? "", match),
+				text: getMatches(text ?? "", match)
+			};
+		});
 }
 
 function getMatches(text: string, searchTerm: string, limit = 1) {
-    const regex = new RegExp(searchTerm, 'gi');
-    const indexes = [];
-    let matches = 0;
-    let match;
+	const regex = new RegExp(searchTerm, "gi");
+	const indexes = [];
+	let matches = 0;
+	let match;
 
-    while ((match = regex.exec(text)) !== null && matches < limit) {
-        indexes.push(match.index);
-        matches++
-    }
+	while ((match = regex.exec(text)) !== null && matches < limit) {
+		indexes.push(match.index);
+		matches++;
+	}
 
-    return indexes.map((index) => {
-        const start = index - 20;
-        const end = index + 50;
-        const excerpt = text.substring(start, end).trim()
+	return indexes.map((index) => {
+		const start = index - 20;
+		const end = index + 50;
+		const excerpt = text.substring(start, end).trim();
 
-        return `...${replaceTextWithMarker(excerpt, searchTerm)}...`;
-    });
-} 
+		return `...${replaceTextWithMarker(excerpt, searchTerm)}...`;
+	});
+}
 
 function replaceTextWithMarker(text: string, match: string) {
-    const regex = new RegExp(match, 'gi');
-    // oh hi mark
-    return text.replaceAll(regex, (match) => `<mark>${match}</mark>`);
+	const regex = new RegExp(match, "gi");
+	// oh hi mark
+	return text.replaceAll(regex, (match) => `<mark>${match}</mark>`);
 }
--- src/routes/search.json/+server.ts
+++ src/routes/search.json/+server.ts
@@ -1,109 +1,111 @@
-import { json } from '@sveltejs/kit'
-import { getPosts } from '$lib/postUtils'
-import { getRecipes } from '$lib/recipeUtils';
-import type { PostMetadata, SearchItem } from '$lib/types';
-import matter from 'gray-matter';
+import { json } from "@sveltejs/kit";
+import type { SearchItem } from "$lib/types";
+import matter from "gray-matter";
 
 const patterns: Record<string, RegExp> = {
-    frontmatter: /---.*?---/gs,
-    code: /```.*?\n|```/gs,
-    inline: /`([^`]*)`/g,
-    heading: /^#{1,6}\s.*$/gm,
-    link: /\[([^\]]+)\]\(([^)]+)\)/g,
-    image: /\!\[.*?\]\(.*?\)/g,
-    blockquote: /> /gm,
-    bold: /\*\*/g,
-    italic: /\b_([^_]+)_(?!\w)/g,
-    special: /{%.*?%}/g,
-    script: /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
-    tags: /[<>]/g,
-}
+	frontmatter: /---.*?---/gs,
+	code: /```.*?\n|```/gs,
+	inline: /`([^`]*)`/g,
+	heading: /^#{1,6}\s.*$/gm,
+	link: /\[([^\]]+)\]\(([^)]+)\)/g,
+	image: /!\[.*?\]\(.*?\)/g,
+	blockquote: /> /gm,
+	bold: /\*\*/g,
+	italic: /\b_([^_]+)_(?!\w)/g,
+	special: /{%.*?%}/g,
+	script: /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+	tags: /[<>]/g
+};
 
 const htmlEntities: Record<string, string> = {
-    '<': '&lt;',
-    '>': '&gt;',
-}
+	"<": "&lt;",
+	">": "&gt;"
+};
 
 function stripMarkdown(markdown: string) {
-    for (const pattern in patterns) {
-        switch (pattern) {
-            case 'inline':
-                markdown = markdown.replace(patterns[pattern], '$1')
-                break
-            case 'tags':
-                markdown = markdown.replace(
-                    patterns[pattern],
-                    (match) => htmlEntities[match]
-                )
-                break
-            case 'link':
-                markdown = markdown.replace(patterns[pattern], '$2')
-                break
-            case 'italic':
-                markdown = markdown.replace(patterns[pattern], '$1')
-                break
-            case 'script':
-                markdown = markdown.replace(patterns[pattern], '')
-            default:
-                markdown = markdown.replace(patterns[pattern], '')
-        }
-    }
+	for (const pattern in patterns) {
+		switch (pattern) {
+			case "inline":
+				markdown = markdown.replace(patterns[pattern], "$1");
+				break;
+			case "tags":
+				markdown = markdown.replace(patterns[pattern], (match) => htmlEntities[match]);
+				break;
+			case "link":
+				markdown = markdown.replace(patterns[pattern], "$2");
+				break;
+			case "italic":
+				markdown = markdown.replace(patterns[pattern], "$1");
+				break;
+			case "script":
+				markdown = markdown.replace(patterns[pattern], "");
+				break;
+			default:
+				markdown = markdown.replace(patterns[pattern], "");
+		}
+	}
 
-    return markdown
+	return markdown;
 }
 
-
 export async function GET() {
-    let fulllist: SearchItem[] = [];
-    let exceptions = [];
+	const fulllist: SearchItem[] = [];
+	const exceptions = [];
 
-    try {
+	try {
+		const paths = import.meta.glob("/src/lib/posts/*.md", {
+			import: "default",
+			query: "?raw",
+			eager: true
+		});
+		const entries = Object.entries(paths)
+			.map(([k, content]) => {
+				const frontmatter = matter(content as string);
+				if (!frontmatter.data.hidden) {
+					return {
+						category: "Statement",
+						title: frontmatter.data.title,
+						slug: "/blog/" + k.replace("/src/lib/posts/", "").replace(".md", ""),
+						description: frontmatter.data.description,
+						text: stripMarkdown(content as string)
+					};
+				}
+			})
+			.filter(Boolean);
 
-        const paths = import.meta.glob("/src/lib/posts/*.md", { import: 'default', query: '?raw', eager: true });
-        const entries = Object.entries(paths)
-            .map(([k, content]) => {
-                const frontmatter = matter(content as string);
-                if (!frontmatter.data.hidden) {
-                    return {
-                        category: 'Statement',
-                        title: frontmatter.data.title,
-                        slug: '/blog/' + k.replace("/src/lib/posts/", "").replace(".md", ""),
-                        description: frontmatter.data.description,
-                        text: stripMarkdown(content as string)
-                    }
-                }
-            })
-            .filter(Boolean);
+		const recipePath = import.meta.glob("/src/lib/posts/recipes/*.md", {
+			import: "default",
+			query: "?raw",
+			eager: true
+		});
+		const recipes = Object.entries(recipePath)
+			.map(([k, content]) => {
+				const frontmatter = matter(content as string);
+				if (!frontmatter.data.hidden) {
+					return {
+						category: "Recipe",
+						title: frontmatter.data.title,
+						slug: "/fite/recipes/" + k.replace("/src/lib/posts/recipes/", "").replace(".md", ""),
+						description: frontmatter.data.description,
+						text: stripMarkdown(content as string)
+					};
+				}
+			})
+			.filter(Boolean);
 
-        const recipePath = import.meta.glob("/src/lib/posts/recipes/*.md", { import: 'default', query: '?raw', eager: true });
-        const recipes = Object.entries(recipePath)
-            .map(([k, content]) => {
-                const frontmatter = matter(content as string);
-                if (!frontmatter.data.hidden) {
-                    return {
-                        category: 'Recipe',
-                        title: frontmatter.data.title,
-                        slug: '/fite/recipes/' + k.replace("/src/lib/posts/recipes/", "").replace(".md", ""),
-                        description: frontmatter.data.description,
-                        text: stripMarkdown(content as string)
-                    }
-                }
-            })
-            .filter(Boolean);
+		entries.forEach((e) => {
+            if(e)
+			    fulllist.push(e);
+		});
 
-        entries.forEach((e) => {
-            fulllist.push(e);
-        });
+		recipes.forEach((r) => {
+            if(r)
+			    fulllist.push(r);
+		});
 
-        recipes.forEach((r) => {
-            fulllist.push(r);
-        });
-
-        //return json(entries);
-        return json(fulllist);
-
-    } catch (error) {
-        console.error('Error fetching posts and recipes:', error);
-        exceptions.push(json({ error: 'Failed to fetch posts and recipes' }, { status: 500 }));
-    }
+		return json(fulllist);
+	} catch (error) {
+		console.error("Error fetching posts and recipes:", error);
+		exceptions.push(json({ error: "Failed to fetch posts and recipes" }, { status: 500 }));
+	}
 }
